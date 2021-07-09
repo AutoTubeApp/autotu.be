fbd31f54eaa88cad82e29dce65fcda17d8332756
Stéphane Depierrepont <stephane@dpp.st>
2021-07-09 12:13:34 +0200

    component VideoThumb.vue

---
+++ components/VideoThumb.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="pr-2 pl-2 ">
+    <v-row class="d-flex pointer">
+      <v-img
+        :src="imgSrc"
+        :lazy-src="imgLazySrc"
+        aspect-ratio="16/9"
+        class="grey lighten-2"
+        @mouseover="handleImgHover"
+        @mouseout="handleImgOut"
+      >
+        <template #placeholder>
+          <v-row
+            class="fill-height ma-0"
+            align="center"
+            justify="center"
+          >
+            <v-progress-circular
+              indeterminate
+              color="grey lighten-5"
+            />
+          </v-row>
+        </template>
+      </v-img>
+    </v-row>
+    <v-row class="d-flex pt-3">
+      <v-col cols="auto" class="ma-0">
+        <v-avatar size="36">
+          <img
+            alt="toorop"
+            src="dev/avatar.jpg"
+          >
+        </v-avatar>
+      </v-col>
+      <v-col class="pl-0">
+        <h4>J'ai acheté un van !!!</h4>
+        <a class="subtitle" spellcheck="false" href="/c/toorop" dir="auto">Toorop</a>
+        <p class="subtitle">
+          10K vues &#8226; Il y à 16 heures
+        </p>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class VideoThumb extends Vue {
+  readonly imgSrcDefault: string = '/dev/thumbnail.webp'
+  readonly imgSrcPreviewDefault: string = '/dev/thumbnail-preview.webp'
+
+  imgSrc: string = this.imgSrcDefault
+  imgLazySrc: string = this.imgSrcDefault
+
+  handleImgHover (): void {
+    this.imgSrc = this.imgSrcPreviewDefault
+  }
+
+  handleImgOut (): void {
+    this.imgSrc = this.imgSrcDefault
+  }
+}
+</script>
+
+<style scoped>
+.subtitle {
+  font-size: 14px;
+  color: #aaa !important;
+  text-decoration: none;
+  line-height: 1em;
+}
+
+.pointer {
+  cursor: pointer;
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,54 +1,20 @@
 <template>
-  <v-row class="d-flex pl-6">
+  <v-row class="d-flex mt-3 ml-2 mr-2">
     <v-col
       v-for="n in 9"
       :key="n"
-      class="child-flex mb-5 pt-5 pr-1"
+      class="child-flex"
       cols="12"
       md="3"
       sm="6"
     >
-      <v-row class="d-flex">
-        <v-img
-          src="/dev/thumbnail.webp"
-          lazy-src="/dev/thumbnail.webp"
-          aspect-ratio="16/9"
-          class="grey lighten-2"
-        >
-          <template #placeholder>
-            <v-row
-              class="fill-height ma-0"
-              align="center"
-              justify="center"
-            >
-              <v-progress-circular
-                indeterminate
-                color="grey lighten-5"
-              />
-            </v-row>
-          </template>
-        </v-img>
-      </v-row>
-      <v-row class="d-flex pt-3">
-        <v-col cols="auto" class="ma-0">
-          <v-avatar size="36">
-            <img
-              alt="toorop"
-              src="dev/avatar.jpg"
-            >
-          </v-avatar>
-        </v-col>
-        <v-col class="pl-0">
-          <h4>J'ai acheté un van !!!</h4>
-          <a href="" dir="auto">Toorop</a>
-          <p>10K vues &#8226; Il y à 16 heures</p>
-        </v-col>
-      </v-row>
+      <VideoThumb />
     </v-col>
   </v-row>
 </template>
 
-<script>
+<script lang="ts">
+
 export default {
   name: 'Index'
 }
