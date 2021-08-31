133136cb32022e6ab4fc6fd00d98ac8f4fa3846a
Stéphane Depierrepont <toorop@gmail.com>
2021-08-31 16:10:04 +0200

    pm2 script

--- update.sh
+++ update.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
-
-set -e;
-ssh-agent bash;
-ssh-add /root/.ssh/id_rsa_gh;
-git reset --hard;
-git pull;
-npm i;
-npm run build;
-pm2 restart autotube;
+set -e
+git reset --hard
+ssh-agent bash -c 'ssh-add /root/.ssh/id_rsa_gh; git pull'
+npm i
+npm run build
+pm2 restart autotube
