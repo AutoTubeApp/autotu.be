7ffefa21ab2c11f6221cdcb85f23d4d53ad6a586
Stéphane Depierrepont <toorop@gmail.com>
2021-09-29 10:39:47 +0200

    add video load manifest

--- api/handlers/video.ts
+++ api/handlers/video.ts
@@ -1,10 +1,11 @@
 /*
-    Related to videos
+    Handlers related to videos
  */
 
 import express from 'express'
 import validator from 'validator'
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
+
 import { ApiResponse } from '../classes/ApiResponse'
 
 // Checks if mpd exists
@@ -12,16 +13,15 @@ import { ApiResponse } from '../classes/ApiResponse'
 export const getVideoMetaFromManifest = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
   const response = new ApiResponse()
 
-  let { manifest } = req.body
+  const { manifest } = req.body
 
   // https://v.autotube.app/DC6FirstLanding/dash.mpd
   // ECONNREFUSED
   // manifest = 'v.autotube.app/DC6FirstLanding/dash.mpd'
-  manifest = 'https://v.autotube.app/DC6FirstLandingo/dash.mpd'
+  // manifest = 'https://v.autotube.app/DC6FirstLandingo/dash.mpd'
 
   // validate is URL && extension is .mpd
   if (!validator.isURL(manifest)) {
-    console.log('nor an URL')
     res.locals.response = response.setResponse(400, 'URL is not valid', 1,
       `getVideoMetaFromManifest failed: bad URL for manifest: ${manifest}`)
     next()
@@ -36,9 +36,25 @@ export const getVideoMetaFromManifest = async (req: express.Request, res: expres
   }
 
   // load mpd file
-  const r = await axios.get(manifest)
-  // todo check r
-  console.log(r.status)
+  let r: AxiosResponse
+  try {
+    r = await axios.get(manifest)
+  } catch (e) {
+    // 404 not found
+    if (e.response.status === 404) {
+      res.locals.response = response.setResponse(400, 'Remote server reply with a "Not Found" error (404). Check your URL.', 1,
+        `getVideoMetaFromManifest failed: get 404 from remote server: ${manifest}`)
+    } else {
+      res.locals.response = response.setResponse(400, `Remote server reply with a error ${e.reponse.statusText}. Check your URL.`, 1,
+        `getVideoMetaFromManifest failed: get ${e.reponse.status} - ${e.response.statusText} from remote server: ${manifest}`)
+    }
+    next()
+    return
+  }
+
+  // OK manifest exists
+  // get root path
+  console.log(r)
 
   res.status(200).send()
 }
